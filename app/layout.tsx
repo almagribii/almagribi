65d5fa81c7d40b1e1181e58adc3b64ecf539bb00
Almagribii <brucadalm@gmail.com>
2025-12-11 17:05:54 +0700

    fived

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          
           {children}
         </ThemeProvider>
       </body>
