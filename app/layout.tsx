b4a833c18cdca2ab0d2101ba235a8ee7d7b85780
Almagribii <brucadalm@gmail.com>
2025-12-16 21:35:35 +0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,43 @@ import "./globals.css";
 import { Merriweather } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "Brucad Al Magribi",
-  description: "Fullstack Developer",
-  icons: {
-    icon: '/brucad.jpeg', 
+  title: "Brucad Al Magribi | Fullstack Developer & Next.js Expert",
+  description:
+    "Explore the portfolio of Brucad Al Magribi, a skilled Fullstack Developer specializing in modern web technologies like Next.js, React, and serverless architectures.",
+  metadataBase: new URL("https://www.almagribi.my.id"),
+
+  openGraph: {
+    title: "Brucad Al Magribi, Fullstack Developer",
+    description: "Specializing in Next.js, React, and modern web development.",
+    url: "https://www.almagribi.my.id", 
+    siteName: "Brucad Al Magribi Portfolio",
+    images: [
+      {
+        url: "/brucad-social.jpeg", 
+        width: 1200,
+        height: 630,
+        alt: "Brucad Al Magribi - Fullstack Developer Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
+
+
+
+  icons: {
+    icon: "/brucad.jpeg", 
+    apple: "/apple-touch-icon.png", 
+  },
+
+  keywords: [
+    "Fullstack Developer",
+    "Next.js",
+    "React",
+    "TypeScript",
+    "Web Development",
+    "Portfolio",
+  ],
 };
 
 const merriweather = Merriweather({
